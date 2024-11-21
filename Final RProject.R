f0c5853127936e21ccf4e9a5fd79238f80f78779
OStuart0608 <10840508@uvu.edu>
2024-11-21 14:30:19 -0700

    3rd test

--- Final RProject.R
+++ Final RProject.R
@@ -11,31 +11,58 @@ library(MASS)
 Final_Project_Data <- read.csv("C:/Users/Owner/Desktop/Biol 3100/OStuart0608.github.io/IHME_GBD_2010_MORTALITY_AGE_SPECIFIC_BY_COUNTRY_1970_2010 (1).csv")
 view(Final_Project_Data)
 
-summary(Final_Project_Data)
+
+
+
+Final_Project_Data1 <- Final_Project_Data %>%
+  filter(Country.Name == c('Australia', 'Brazil', 'Central Africa', 'Canada', 'China', 'France', 'United Kingdom', 'Japan', 'Mexico', 'United States')) %>%
+  view()
+
+
+
+Final_Project_Data2 <- aggregate(Number.of.Deaths ~ Country.Name, data = Final_Project_Data1, FUN = max)
+view(Final_Project_Data2) 
+
+
+
+
+
+summary(Final_Project_Data1)
 names(Final_Project_Data)
 
-Final_Project_Data %>%
+Final_Project_Data2 %>% 
   ggplot(mapping = aes(x = Country.Name, 
                        y = Number.of.Deaths,
   )) + 
   geom_bar(stat = 'identity') +
-labs(title = 'Death Rates per Year',
-     x = 'Year',
-     y = 'Death Rate per 100000 ') + 
-  theme(axis.text.x = element_text(angle=90, hjust = 1 ))
+labs(title = 'Deaths per Country',
+     x = 'Country',
+     y = 'Death Max') +
+  theme(axis.text.x = element_text(angle=45, hjust = 1 ))
 
-Final_Project_Data %>%
-  ggplot(aes(x = Year, y = Death.Rate.Per.100.000))+
+
+Final_Project_Data3<- aggregate(Number.of.Deaths ~ Year,data = Final_Project_Data1, FUN = max)
+view(Final_Project_Data3)
+
+
+as.character(Final_Project_Data3$Year)
+
+
+Final_Project_Data3 %>%
+  ggplot(mapping = aes(x = Year,
+                       y = Number.of.Deaths,
+  )) + 
   geom_bar(stat = 'identity') +
   labs(title = 'Death Rates per Year',
        x = 'Year',
-       y = 'Death Rate')
+       y = 'Death Max') +
+  theme(axis.text.x = element_text(angle=45, hjust = 1 ))
 
 
 
 
-
-
+Final_Project_Data3<- aggregate(Deathsr,data = Final_Project_Data1, FUN = max)
+view(Final_Project_Data3)
 
 
 
