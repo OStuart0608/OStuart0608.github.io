975b2ede7e855d8c428ad07dd5dade7771124bf1
OStuart0608 <10840508@uvu.edu>
2024-11-29 16:57:45 -0700

    7th Website

--- Final RProject.R
+++ Final RProject.R
@@ -12,9 +12,8 @@ Final_Project_Data <- read.csv("C:/Users/Owner/Desktop/Biol 3100/OStuart0608.git
 view(Final_Project_Data)
 
 
-Final_Project_Data1 <- Final_Project_Data %>%
+Final_Project_Data6 <- Final_Project_Data5 %>%
   filter(Country.Name == c('Australia', 'Brazil', 'Central Africa', 'Canada', 'China', 'France', 'United Kingdom', 'Japan', 'Mexico', 'United States')) %>%
-  select(-Country.Code) %>%
   view()
 
 Final_Project_Data1 <- Final_Project_Data
@@ -27,7 +26,7 @@ view(Final_Project_Data1)
   Final_Project_Data5 <-  Final_Project_Data1$Number.of.Deaths <- as.numeric(gsub(",", "", Final_Project_Data1$Number.of.Deaths)) %>%
     view()
   
-  write.csv(Final_Project_Data1, "Final_Project_Data1.csv", row.names = FALSE)
+  write.csv(Final_Project_Data6, "Final_Project_Data6.csv", row.names = FALSE)
   
   
   
@@ -109,108 +108,98 @@ ggplot(Final_Project_Data4, aes(x = Sex, y = Number.of.Deaths)) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
 
+## Creating a Statistical model
+
+names(Final_Project_Data6)
+
+Final_Project_mod <- glm(data = Final_Project_Data6,
+                  formula = Death.Rate.Per.100.000 ~ Country.Name + Year)
+summary(Final_Project_mod)
+
+
+
+Final_Project_mod1 <- glm(data = Final_Project_Data6,
+                         formula = Death.Rate.Per.100.000 ~ Country.Name + Age.Group)
+summary(Final_Project_mod1)
+
+
+
+Final_Project_mod2 <- glm(data = Final_Project_Data6,
+                          formula = Death.Rate.Per.100.000 ~ Country.Name  + Sex)
+summary(Final_Project_mod2)
+
+
+Final_Project_mod3 <- glm(data = Final_Project_Data6,
+                          formula = Death.Rate.Per.100.000 ~ Country.Name  + Year + Age.Group + Sex)
+summary(Final_Project_mod3)
+
+
+compare_performance(Final_Project_mod, Final_Project_mod1, Final_Project_mod2, Final_Project_mod3) %>% plot()
+
+library(broom)
+
+broom::tidy(Final_Project_mod3) 
+
+
+## Creating prediction values
+
+library(modelr)
+Final_Project_Predictions <- add_predictions(Final_Project_Data6, Final_Project_mod3)
+
+view(Final_Project_Predictions)
+
+
+
+
+## 7. plots these predictions alongside the real data
+library(ggplot2)
+library(tidyverse)
+library(modelr)
+library(dplyr)
+library(broom)
+library(kableExtra)
+library(easystats)
 
 
 
 
 
 
+     par(mfrow = c(1, 1))
+     
+     # Plot 1: Predictions vs Death ate
+     plot(Final_Project_Predictions$pred, 
+          Final_Project_Predictions$Death.Rate.Per.100.000, 
+          xlab = "Prediction", 
+          ylab = "Death Rate Per 100,000")
+     
+broom
 
-
-
-
-Final_Project_Data4<- aggregate(Number.of.Deaths ~ Sex,data = Final_Project_Data1, FUN = max)
-view(Final_Project_Data4)
-
-
-as.character(Final_Project_Data3$Year)
-
-
-Final_Project_Data4 %>%
-  ggplot(mapping = aes(x = Sex,
-                       y = Number.of.Deaths,
-  )) + 
-  geom_bar(stat = 'identity') +
-  labs(title = 'Death Rates per Year',
-       x = 'Sex',
-       y = 'Death Max') +
-  theme(axis.text.x = element_text(angle=45, hjust = 1 ))
-
-Final_Project_Data3<- aggregate(Number.of.Deaths ~ Year, data = Final_Project_Data1, FUN = max)
-view(Final_Project_Data3)
-
-head(Final_Project_Data3)
-
-Final_Project_Data3$Year <- as.character(Final_Project_Data3$Year)
-Final_Project_Data3$Number.of.Deaths <- as.character(Final_Project_Data3$Number.of.Deaths)
-sapply(Final_Project_Data3, class)
-
-view(Final_Project_Data3)
-Final_Project_Data3 %>%
-  ggplot(mapping = aes(x = Year,
-                       y = Number.of.Deaths,
-  )) + 
-  geom_bar(stat = 'identity') +
-  labs(title = 'Death Rates per Year',
-       x = 'Year',
-       y = 'Death Max') +
-  theme(axis.text.x = element_text(angle=45, hjust = 1 ))
-
-
-Final_Project_Data4 %>%
-  ggplot(mapping = aes(x = Sex,
-                       y = Number.of.Deaths,
-  )) + 
-  geom_bar(stat = 'identity') +
-  labs(title = 'Death Rates per Year',
-       x = 'Sex',
-       y = 'Death Max') +
-  theme(axis.text.x = element_text(angle=45, hjust = 1 ))
-
-Final_Project_Data5 <- Final_Project_Data1
-
-
-Final_Project_Data5$Year <- as.character(Final_Project_Data5$Year)
-sapply(Final_Project_Data5, class)
-
-view(Final_Project_Data5)
-
-
-Final_Project_Data5 %>%
-  ggplot(mapping = aes(x = Year,
-                       y = Number.of.Deaths,
-  )) + 
-  geom_bar(stat = 'identity') +
-  facet_wrap(~Country.Name)
-  labs(title = 'Death Rates per Year',
-       x = 'Year',
-       y = 'Number of Deaths') +
-  theme(axis.text.x = element_text(angle=45, hjust = 1 ))
+     summary(Final_Project_Predictions)
+     broom::tidy(Final_Project_Predictions)
 
 
 
 
 
 
+     write.csv(Final_Project_Predictions, "Final_Project_Predictions.csv", row.names = FALSE)
 
 
+     # Check the structure of the object
+     str(Final_Project_Predictions)
+     
+     # Convert factors/characters to numeric if necessary
+     Final_Project_Predictions2 <- lapply(Final_Project_Predictions, function(x) {
+       if (is.character(x) || is.factor(x)) as.numeric(as.character(x)) else x
+     })
+     
+     # Try again with broom::tidy()
+     broom::tidy(Final_Project_Predictions2)
+     
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+   
 
 
 
